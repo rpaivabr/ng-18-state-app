53b6d0d896b541fe26ac08625924868a9a687247
Rafael Paiva <r.paivabr@gmail.com>
2024-06-23 20:40:56 -0300

    part 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from './models/product';
 import { Cart, CartItem } from './models/cart';
+import { HttpClient } from '@angular/common/http';
+// import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,36 +11,49 @@ import { Cart, CartItem } from './models/cart';
 })
 export class AppComponent implements OnInit {
   products: Product[] = [];
+  selectedProduct: Product | null = null;
   cart: Cart = {
     cartId: new Date().getTime(),
     cartItems: [],
   };
-  totalItems: number = 0;
+
+  get totalItems(): number {
+    return this.cart.cartItems.length
+  }
+
+  constructor(private httpClient: HttpClient) {}
 
   ngOnInit(): void {
-    fetch('http://localhost:3000/api/v1/coffees')
-      .then((res) => res.json())
-      .then((products: Product[]) => {
+    this.httpClient
+      .get<Product[]>('http://localhost:3000/api/v1/coffees')
+      .subscribe((products: Product[]) => {
         this.products = products;
       });
+    // this.products = await firstValueFrom(products$)
+
+  }
+
+  selectProduct(product: Product | null): void {
+    this.selectedProduct = product;
   }
 
   addToCart(product: Product): void {
     console.log(product);
-    // const newCartItem: CartItem = {
-    //   itemId: product.id,
-    //   productName: product.name,
-    //   unitPrice: product.price,
-    //   quantity: 1,
-    // };
-    // this.cart.cartItems.push(newCartItem);
+    const newCartItem: CartItem = {
+      itemId: product.id,
+      productName: product.name,
+      unitPrice: product.price,
+      quantity: 1,
+    };
+    this.cart.cartItems.push(newCartItem);
     // this.totalItems = this.cart.cartItems.length;
   }
 
-  // resetCart(): void {
-  //   this.cart = {
-  //     cartId: new Date().getTime(),
-  //     cartItems: [],
-  //   };
-  // }
+  resetCart(): void {
+    this.cart = {
+      cartId: new Date().getTime(),
+      cartItems: [],
+    };
+    // this.totalItems = this.cart.cartItems.length;
+  }
 }
